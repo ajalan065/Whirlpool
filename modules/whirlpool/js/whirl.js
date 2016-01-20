ae76140d50946d8a2c99c58a624cba913e0fa8dc
ajalan065 <arpit.jalan@innoraft.com>
2016-01-20 19:07:40 +0530

    Changed js and index file

--- modules/whirlpool/js/whirl.js
+++ modules/whirlpool/js/whirl.js
@@ -1,10 +1,9 @@
-$(window).load(function() {
-
+(function($, Drupal, window, document, undefined) {
+  $(window).load(function() {
     $('#vortex').vortex({
       speed : 60,
       manualAdvance : false,
       initialPosition : 270,
-
     });
-
   });
+})(jQuery, Drupal, this, this.document);
